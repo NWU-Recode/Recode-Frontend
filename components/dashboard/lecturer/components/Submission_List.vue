6dc8859ee5ce82468454eb016418d303459b9e5e
Dylon <edafrika07@gmail.com>
2025-09-12 13:48:49 +0200

    Initialization of L_Analytics components

---
+++ components/dashboard/lecturer/components/Submission_List.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    const submissions = [{
+        name: '',
+        studentNo: null,
+        topic: '',
+        badge: '',
+        time: null
+    }]
+
+</script>
+
+<template>
+    <table>
+        <thead>
+            <tr>
+            <th>Name</th>
+            <th>Student No</th>
+            <th>Topic</th>
+            <th>Badge</th>
+            <th>Time</th>
+            </tr>
+        </thead>
+    </table>
+</template>
+
+<style scoped>
+    
+</style>
