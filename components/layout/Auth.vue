fd1950077f4a6960bf1a9143af324dc1f53753cb
jvranneme37 <jvranneme37@gmail.com>
2025-10-07 02:41:54 +0200

    Update Auth.vue

--- components/layout/Auth.vue
+++ components/layout/Auth.vue
@@ -9,18 +9,34 @@ defineProps<{
     class="relative flex flex-col items-center justify-center px-4 h-dvh lg:max-w-none lg:flex-row lg:px-0"
     :class="{ 'lg:flex-row-reverse': reverse }"
   >
-    <!-- Logo / Image -->
-    <div class="relative z-20 flex justify-center lg:mb-0 lg:ml-6 lg:mr-6">
-      <img
-        class="h-48 w-48 lg:h-[600px] lg:w-[600px] md:h-[400px] md:w-[400px] sm:h-72 sm:w-72"
-        src="/logo.svg"
-        alt="Logo"
-      >
-    </div>
+    <Transition name="slide">
+      <div class="relative z-20 flex justify-center lg:mb-0 lg:ml-6 lg:mr-6">
+        <img
+          class="h-48 w-48 lg:h-[600px] lg:w-[600px] md:h-[400px] md:w-[400px] sm:h-72 sm:w-72"
+          src="/logo.svg"
+          alt="Logo"
+        >
+      </div>
+    </Transition>
 
-    <!-- Form / Slot -->
     <div class="mx-auto max-w-md w-full flex-1 lg:p-8">
       <slot />
     </div>
   </div>
 </template>
+
+<style scoped>
+.slide-enter-from {
+  transform: translateX(-100%);
+  opacity: 0;
+}
+
+.slide-enter-active {
+  transition: all 0.7s ease-out;
+}
+
+.slide-enter-to {
+  transform: translateX(0);
+  opacity: 1;
+}
+</style>
